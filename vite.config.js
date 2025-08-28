4cec54e873f401cd9fb1b9a34b7d32c4f28c251e
rberiot <remy.beriot@gmail.com>
2025-08-28 14:48:42 +0200

    fixed path again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mkab_track_picker/",
+  base: "/mkab_track_picker/dist/",
   plugins: [
     vue(),
     vueDevTools(),
